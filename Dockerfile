90ad3502e3dc515213751e723f554d46f5cd6be8
duongvm <duongvm@hblab.vn>
2023-07-25 18:22:42 +0700

    git action

--- Dockerfile
+++ Dockerfile
@@ -10,9 +10,10 @@ CMD [ "npm", "run", "dev" ]
 FROM node:18-alpine AS prod
 WORKDIR /usr/src/app
 COPY --chown=node:node package*.json ./
-COPY . ./
 COPY --chown=node:node . .
 RUN npm i --only=production && chown -R node:node node_modules
+RUN mkdir dist && chown -R node:node dist
+RUN mkdir logs && chown -R node:node dist
 USER node
 EXPOSE 3000
 RUN npm run build
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,10 +1,10 @@
 import express from 'express';
 import userController from '../controllers/user.controller';
-import { checkAdminRole, checkProfileAccess } from '../middleware/permission.middleware';
+import { checkProfileAccess } from '../middleware/permission.middleware';
 
 const userRouter = express.Router();
 
-userRouter.get('/', checkAdminRole, userController.getAll);
+userRouter.get('/', userController.getAll);
 userRouter.get('/:userId', checkProfileAccess, userController.get);
 userRouter.put('/:userId', checkProfileAccess, userController.update);
 userRouter.delete('/:userId', checkProfileAccess, userController.delete);
